42a2626e2d420ef4be2266a0964932c1c25aff63
Julia_Vladomira <juliavladomira@gmail.com>
2023-05-05 16:03:16 +0200

    add model

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+const db = require("./models/index.js");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded());
+app.get("/", function (req, res) {
+   res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(port, () =>
+   console.log(`url-shortener listening on port http://localhost:${port}!`)
+);
