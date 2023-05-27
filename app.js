9f906012ee58dd2a414e3cc0edfc21b5f9ea5312
Julia_Vladomira <juliavladomira@gmail.com>
2023-05-27 15:08:32 +0200

    config

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
-// const sequelize = require("./config/config.json");
 const models = require("./models/index.js");
 const router = require("./routes/index");
 const cors = require("cors");
@@ -14,19 +13,10 @@ router.use(bodyParser.json());
 
 app.use(cors());
 app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.urlencoded());
 app.use("/api", router);
-// app.get("/5", function (req, res) {
-//    return res.sendFile(path.join(__dirname + "/index.html"));
-//    // return res.;
-// });
 
 const start = async () => {
    try {
-      // await sequelize.authenticate();
-      // await sequelize.sync();
-
       app.listen(`${PORT}`, () =>
          console.log(`Hello, I'm a shop on port ${PORT}`)
       );
