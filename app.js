d83a332bee51f21d3699263769df1b04cd84ce66
Julia_Vladomira <juliavladomira@gmail.com>
2023-05-27 15:36:52 +0200

    env changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || "8080";
+const { DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
 
 const bodyParser = require("body-parser");
 router.use(bodyParser.json());
@@ -24,5 +25,5 @@ const start = async () => {
       console.log("my error:", error);
    }
 };
-
 start();
+// postgres://ytojzpomxrwshc:a12782e45b385c1fd188f58df2054ac9a81637d4e289d6ad839cd093c50cd9d9@ec2-34-236-103-63.compute-1.amazonaws.com:5432/ds8fjfmi7cdgm
--- db.js
+++ db.js
@@ -1,14 +1,8 @@
 const { Sequelize } = require("sequelize");
+const { DB_HOST } = process.env;
 
-module.exports = new Sequelize(
-   process.env.DB_NAME,
-   process.env.DB_USER,
-   process.env.DB_PASSWORD,
-   {
-      dialect: "postgres",
-      user: "root",
-      password: "science",
-      host: process.env.DB_HOST || "localhost",
-      PORT: process.env.DB_PORT,
-   }
-);
+module.exports = new Sequelize({
+   host: DB_HOST,
+   dialect: "postgres",
+   logging: false,
+});
