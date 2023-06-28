61bd4eaf9186f8f7e77190c429f06d0aadd03781
Julia_Vladomira <juliavladomira@gmail.com>
2023-06-28 15:39:18 +0200

    add redis

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
+const redis = require("redis");
 const express = require("express");
 const sequelize = require("./db-settings");
 const models = require("./database/models/index.js");
 const router = require("./src/routes/index");
 const cors = require("cors");
 const app = express();
-// const path = require("path");
 const ErrorMidlware = require("./src/midlware/ErrorMidlware");
 const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || "8080";
@@ -26,9 +26,14 @@ app.use(express.json());
 app.use("/api", router);
 app.use(ErrorMidlware);
 
+const client = redis.createClient({
+   url: process.env.DATABASE_URL,
+});
+
 const start = async () => {
    try {
       await sequelize.sync();
+      await client.connect();
       app.listen(`${PORT}`, () =>
          console.log(`Hello, I'm a shop on port ${PORT}`)
       );
