0791914d080badd20f783184625b76aba019d51e
Julia_Vladomira <juliavladomira@gmail.com>
2023-06-29 21:35:43 +0200

    reinstall node_modules

--- db-settings.js
+++ db-settings.js
@@ -2,10 +2,5 @@ const { Sequelize } = require("sequelize");
 const pg = require("pg");
 
 module.exports = new Sequelize(process.env.DATABASE_URL, {
-   // dialect: "postgres",
    dialectModule: pg,
-   // user: process.env.PGUSER,
-   // password: process.env.PGPASSWORD,
-   // host: process.env.PGHOST,
-   // PORT: process.env.PGPORT,
 });
